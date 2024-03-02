db324e9297555923ff71e8adbcefe035b9cb8980
Jacob Adamsky <jeadamsky@wpi.edu>
2024-03-02 00:23:21 -0500

    not sure

--- pyplot/python_plotting/visualizer.py
+++ pyplot/python_plotting/visualizer.py
@@ -31,39 +31,40 @@ class Visualizer:
         
 
     def plot_stat(self, y_data, title, filename, isround, keys):
-        def inverse_exponential(x, a, b, c):
-            return a * np.exp(-b * x) + c
-        
-        x_data = range(0, len(y_data))
-        
-        monetary = sorted(list(self.per_round_player_value_pct.find() if isround else self.per_pick_player_value_pct.find()), key=sort)
-        x_mon = range(0, len(monetary))
-        y_mon = [d["pct"] * 100 for d in monetary]
-        
-        
-        popt, _ = curve_fit(inverse_exponential, x_mon, y_mon)
-        a_opt, b_opt, c_opt = popt
-        x_fit = np.linspace(min(x_mon), max(x_mon), len(x_mon))
-        y_fit = inverse_exponential(x_fit, a_opt, b_opt, c_opt)
-        
-        plt.plot(x_fit, y_fit, 'r-', label='Draft Value Percentage')
-        
-        plt.xlabel("Draft Round" if isround else "Draft Pick Number")
-        plt.ylabel("Percentage (%)")
-        plt.title(title)
-        
-        if isround:
-            plt.ylim(0, 60)
-            plt.xticks(range(len(keys)), keys)
-        else: 
-            plt.ylim(0, 10)
-            
-        plt.bar(x_data, y_data, label=title)
-    
-        plt.legend()
-        plt.savefig(f"{self.__img_dir}{'round' if isround else 'pick'}_{filename}.png")
-        self.clear()
-        plt.close()
+         def inverse_exponential(x, a, b, c):
+             return a * np.exp(-b * x) + c
+     
+         x_data = range(0, len(y_data))
+     
+         monetary = sorted(list(self.per_round_player_value_pct.find() if isround else self.per_pick_player_value_pct.find()), key=sort)
+         x_mon = range(0, len(monetary))
+         y_mon = [d["pct"] * 100 for d in monetary]
+     
+         
+         popt, _ = curve_fit(inverse_exponential, x_data[:-1], y_data[:-1])
+         a_opt, b_opt, c_opt = popt
+         x_fit = np.linspace(min(x_data[::-1]), max(x_data[:-1]), len(x_data[:-1]))
+         y_fit = inverse_exponential(x_fit, a_opt, b_opt, c_opt)
+     
+         plt.plot(x_fit, y_fit, 'r-', label=title)
+     
+         plt.xlabel("Draft Round" if isround else "Draft Pick Number")
+         plt.ylabel("Percentage (%)")
+         plt.title(title)
+     
+         if isround:
+             plt.ylim(0, 60)
+             plt.xticks(range(len(keys)), keys)
+         else: 
+             plt.ylim(0, 10)
+         
+         plt.bar(x_mon, y_mon, label='Draft Value Percentage')
+         plt.scatter(x_data, y_data, label=f"Actual {title}")
+ 
+         plt.legend()
+         plt.savefig(f"{self.__img_dir}{'round' if isround else 'pick'}_{filename}.png")
+         self.clear()
+         plt.close()
     
     def clear(self):
         plt.cla()
